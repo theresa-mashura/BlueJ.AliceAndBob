35345fcce3fb0a24b406cb95188338ac7a15c588
Theresa <theresa@Gbrads-Mac.home>
2021-02-24 22:04:34 -0500

    satisfy unit tests

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -16,7 +16,13 @@ public class AliceAndBobEngine
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-    
+        if(input == "Alice") {
+            result = "Hello Alice";
+        } else if (input == "Bob") {
+            result = "Hello Bob";
+        } else {
+            result = "I DON'T KNOW YOU, YOU ARE SCARY"; 
+        }
         return result;
     }
 }
